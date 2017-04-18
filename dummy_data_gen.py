83f03cb30083ee32c4317d1edb7808b7df21c017
Eli Reiman <ereiman@clarity-us.com>
2017-04-18 16:50:49 -0700

    Update ~/id_file.
    Updates the id_file with the last value from the run.

--- dummy_data_gen.py
+++ dummy_data_gen.py
@@ -9,9 +9,11 @@ from datetime import timedelta
 from faker import Factory
 
 """
+v20170418a
 Example Usage to create json output of customer records, and to test 
     python dummy_data_gen.py -dt customer -json -br
-    python ~/github/datagen/dummy_data_gen.py -dt Sales -json -c 1000 -sd -d ~/tmp -idf /tmp/id_file
+    python ~/github/datagen/dummy_data_gen.py -dt Sales -json -c 1000 -sd -d ~/tmp -idf ~/id_file
+	python /efs/datagen/dummy_data_gen.py -dt Sales -d '/efs/dummydata' -c 2000000 -sd  -idf ~/id_file
 	nohup python dummy_data_gen.py -dt sales -fn sales -c 100000000 &
 		--> 43K rows / seconds * 8 = 344K / seconds
 		--> 80K rows / sec on single micro instance on EFS (while bursting)
@@ -220,6 +222,9 @@ def main():
 		ddg.generate_dummy_data()
 	else:
 		ddg.generate_benchmark_report()
+	final_id = starting_id + args.count
+	cmd = "echo '{}' > ~/id_file".format(final_id)
+	os.system(cmd)
 
 if __name__ == "__main__":
 	main()
