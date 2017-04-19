1b34f1cadf8d3b9e6666885606eda5237a256d53
Eli Reiman <ereiman@clarity-us.com>
2017-04-19 14:41:01 -0700

    Performance enhancement
    removed Faker data components for most data elements -- too slow.  need
    to generate 100M customers!  ;-)

--- datatypes/Customer.py
+++ datatypes/Customer.py
@@ -6,13 +6,13 @@ class Customer(DataTypeBase):
 	def __init__(self, fake, useSystemDate=False):
 		self.ID = Customer.ID
 		Customer.ID += 1
-		self.first_name = fake.first_name()
-		self.last_name = fake.last_name()
-		self.credit_card_number = fake.credit_card_number()
+		self.first_name = ''
+		self.last_name = ''
+		self.credit_card_number = ''
 		#self.billing_address = fake.address().replace('\n', ' ')
 		self.postalcode = fake.postalcode()
-		self.phone_number = fake.phone_number()
-		self.date_joined = str(datetime.now()) if useSystemDate else str(fake.date_time_this_century())
+		self.phone_number = ''
+		self.date_joined = datetime.now() #str(datetime.now()) if useSystemDate else str(fake.date_time_this_century())
 
 	@staticmethod
 	def get_headers(self, delimiter="|"):
@@ -31,3 +31,18 @@ class Customer(DataTypeBase):
 				self.postalcode + delimiter + \
 				self.phone_number + delimiter + \
 				str(self.date_joined)
+
+'''
+#Previous version of Customer data -- removing to make data generation faster
+	def __init__(self, fake, useSystemDate=False):
+		self.ID = Customer.ID
+		Customer.ID += 1
+		self.first_name = fake.first_name()
+		self.last_name = fake.last_name()
+		self.credit_card_number = fake.credit_card_number()
+		#self.billing_address = fake.address().replace('\n', ' ')
+		self.postalcode = fake.postalcode()
+		self.phone_number = fake.phone_number()
+		self.date_joined = str(datetime.now()) if useSystemDate else str(fake.date_time_this_century())
+
+'''
